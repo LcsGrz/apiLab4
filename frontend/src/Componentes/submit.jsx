14391681d5011a83bd60ecf789a6e213984eb61a
altars99 <julyomanzano@gmail.com>
2018-06-18 00:41:11 -0300

    busqueda de usuario falta agregarlo al campo

--- frontend/src/Componentes/submit.jsx
+++ frontend/src/Componentes/submit.jsx
@@ -29,7 +29,8 @@ class Submit extends Component {
             .then((response) => {
                 response.json().then(function (data) {
                     console.log("Al token Perro: "+data.token);
-                    localStorage.setItem('token',JSON.stringify('Authorization: Bearer '+data.token));
+                    // localStorage.setItem('token',JSON.stringify('Authorization: Bearer '+data.token));
+                    localStorage.setItem('token',data.token);
                 });
 
             })
@@ -47,7 +48,7 @@ class Submit extends Component {
         fetch('http://127.0.0.1:420/register', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json'        
             },
             body: JSON.stringify({
                 credentials: {
@@ -72,19 +73,23 @@ class Submit extends Component {
     }
     buscar = () =>{ 
         const { username } = this.props
-        const url = 'http://127.0.0.1:420/usuarios?q={"username":"'+username+'"}';
+        const url = 'http://127.0.0.1:420/api/find';
+        const tokeen = localStorage.getItem('token')
         fetch(url, {
-            method: 'GET',
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
-            }
-            
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+(tokeen)
+            },
+            body: JSON.stringify({               
+                credentials: {
+                    username}
+            })            
         })
             .then((response) => {
-                response.json().then(function (data) {
-                    data.result.map((x)=>{
-                        console.log(x)
-                    })
+                console.log(response)
+                response.json().then(function (data) {                   
+                        console.log(data)                    
                 });
     
             })
